5b6d9c08f258d4474c36742af6545aff68ce3a6f
Yrainnn <2651436751@qq.com>
2025-11-15 15:46:25 +0800

    Remove unused langgraph visualize and stub modules

--- services/orchestrator/langgraph_core/__init__.py
+++ services/orchestrator/langgraph_core/__init__.py
@@ -2,7 +2,7 @@
 
 from . import state_types, llm_tools, utils  # noqa: F401
 from .context import patient_context, item_context  # noqa: F401
-from .graphs import main_graph, subgraph_strategy, subgraph_risk, visualize  # noqa: F401
+from .graphs import main_graph, subgraph_strategy, subgraph_risk  # noqa: F401
 from .nodes import node_init, node_strategy, node_clarify, node_risk, node_update, node_score, node_output  # noqa: F401
 from .template_builder_agent import TemplateBuilderAgent  # noqa: F401
 
@@ -15,7 +15,6 @@ __all__ = [
     "main_graph",
     "subgraph_strategy",
     "subgraph_risk",
-    "visualize",
     "node_init",
     "node_strategy",
     "node_clarify",
